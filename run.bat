7d7c47bb065c2224a8a2b560a68a6acca2c6a6b4
Ayan Ahmad <chaudharyayan100@gmail.com>
2025-07-08 19:54:22 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,13 +1,4 @@
 @echo off
-echo 🔧 Compiling PdfParser.java...
-javac -cp ".;pdfbox-2.0.29.jar" PdfParser.java
-
-if exist PdfParser.class (
-    echo ✅ Compilation successful.
-    echo 🏃 Running PdfParser...
-    java -cp ".;pdfbox-2.0.29.jar" PdfParser
-) else (
-    echo ❌ Compilation failed. Check for errors.
-)
-
+call .venv\Scripts\activate
+uvicorn back-end.app:app --reload
 pause
